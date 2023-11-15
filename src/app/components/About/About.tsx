80173cf97ddd8ade653dd371eb5cba7bb8fa9057
Phill Blake <grizzlyintent@yahoo.com>
2023-11-15 14:32:07 -0500

    fnish about page

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -15,36 +15,42 @@ const background = [
   {
     title: "My Background",
     description:
-      "I have worked in many different industries and have a wide range of skills. I am a self taught developer and have been coding for over 3 years. I am a fast learner and I am always looking to learn new things.",
+      "I have a versatile background, from emergency services to cable construction and appliance repair. As a self-taught freelance web developer for over 3 years, I specialize in JavaScript, React, and blockchain enabled projects. My skills extend from traditional web development to client-side blockchain interaction.",
     component: <Background />,
   },
   {
-    title: "AAA Roadside Assistance",
+    title: "Emergency Roadside Driver",
+    years: "2018 - 2019",
     description:
-      "I was responsible for diagnosing and repairing vehicles that were broken down on the side of the road. Pulling vehicles out from ditches, changing tires, and jump starting vehicles were also part of my job description. I was also responsible for keeping my truck and tools clean and organized.",
+      "Excelled in managing high-stakes emergencies, swiftly diagnosing issues, and assisting customers under pressure. Conducted on-the-spot battery replacements, tire changes, and fuel deliveries on busy interstates, showcasing adaptability and quick problem-solving skills.",
     component: <AAA />,
   },
   {
-    title: "White Mountain Cable Construction",
+    title: "Aerial Fiber Optic Lineman",
+    years: "2019 - 2020",
     description:
-      "I installed telephone poles and fiber optic cable both aerial and underground.",
+      "I operated as an Aerial Lineman, specializing in the intricate work of installing and maintaining fiber optic lines. Climbing telephone poles, maneuvering bucket trucks, and executing precise installations in various environments were second nature. I faced challenges head-on, from clearing trees off lines to installing underground cables, showcasing my meticulous approach to complex tasks.",
     component: <LineWork />,
   },
   {
     title: "Appliance Repair Technician",
-    description: "I diagnosed and repaired appliances in customers homes.",
+    years: "2020 - 2021",
+    description:
+      "In my role as an Appliance Repair Technician at Sears Home Services, I delved into diagnosing and repairing a diverse range of equipment. I honed expertise in hands-on troubleshooting, meticulously executing precise part replacements to ensure the effective resolution of appliance issues.",
     component: <Appliance />,
   },
   {
-    title: "Freelance Blockchain Developer",
+    title: "Freelance Developer",
+    years: "2021 - Present",
     description:
-      "I taught myself how to code with Udemy, Youtube, StackOverflow, and other online resources. I have worked on over 15 projects for multiple clients from around the globe.",
+      "As a freelance Frontend and Blockchain Developer, I fused creativity in crafting user interfaces with expertise in blockchain technology. I navigated dynamic landscapes, honing technical prowess to deliver tailored solutions in web and decentralized systems.",
     component: <Dev />,
   },
 ];
 
 export default function About() {
   const [step, setStep] = useState(0);
+  const [animate, setAnimate] = useState(false);
 
   const handleNext = () => {
     if (step === background.length - 1) {
@@ -52,14 +58,11 @@ export default function About() {
     } else {
       setStep(step + 1);
     }
-  };
+    setAnimate(true);
 
-  const handlePrev = () => {
-    if (step === 0) {
-      setStep(background.length - 1);
-    } else {
-      setStep(step - 1);
-    }
+    setTimeout(() => {
+      setAnimate(false);
+    }, 1000);
   };
 
   return (
@@ -104,18 +107,31 @@ export default function About() {
       </div>
 
       <div className={styles.gridRight}>
-        <button className={styles.prevButton} onClick={handlePrev}>
-          Prev
-        </button>
-        <button className={styles.nextButton} onClick={handleNext}>
-          Next
-        </button>
+        <button className={styles.nextButton} onClick={handleNext}></button>
         <div className={styles.aboutSphereUpper}>
           {background[step].component}
         </div>
         <div className={styles.aboutSphereLower}>
-          <h3>{background[step].title}</h3>
-          <p>{background[step].description}</p>
+          <div
+            className={!animate ? styles.details : styles.details + " animate"}
+          >
+            <h2>{background[step].title}</h2>
+            <h4>{background[step].years}</h4>
+          </div>
+          <p className={!animate ? "" : "animate"}>
+            {background[step].description}
+          </p>
+          <button className={styles.nextButtonMobile} onClick={handleNext}>
+            NEXT
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" />
+            </svg>
+          </button>
         </div>
       </div>
     </section>
