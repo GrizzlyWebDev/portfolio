20cf803512ab180714616ebcffaf737938879555
Phill Blake <grizzlyintent@yahoo.com>
2023-11-20 08:42:29 -0500

    finish projects page

--- src/app/components/Projects/Projects.tsx
+++ src/app/components/Projects/Projects.tsx
@@ -27,7 +27,7 @@ const projects = [
     description:
       "A page for a large exchange that started on Avalanche network. This page allows users to launch their own token on the platform.",
     tools: ["next.js", "Typescript", "Tailwind"],
-    link: "https://www.google.com",
+    link: "https://app.swapsicle.io/launchpad?status=finished",
   },
   {
     title: "Quirk n' Co. Donuts",
@@ -35,7 +35,7 @@ const projects = [
     description:
       "A custom Ecommerce site for a donut shop. I created one custom plugin that enables coupon discounts for Quirkie and Quirkilings NFT holders, and another that enables payment with cryptocurrency.",
     tools: ["WooCommerce", "Elementor", "JavaScript", "PHP"],
-    link: "https://www.google.com",
+    link: "https://quirkncodonuts.com/",
   },
   {
     title: "Metaverse Expo",
@@ -43,7 +43,7 @@ const projects = [
     description:
       "A website for a Metaverse Expo that took place in 2022 that was hosted by TCG World.",
     tools: ["React.js", "JavaScript", "Tailwind"],
-    link: "https://www.google.com",
+    link: "https://themetaverseexpo.io/",
   },
   {
     title: "REV3AL Dashboard",
@@ -51,7 +51,7 @@ const projects = [
     description:
       "A dashboard for a blockchain security company that allows users to view stats on the REV3AL token, stake their tokens, farm, and more.",
     tools: ["React.js", "JavaScript", "wagmi.sh", "ethers.js", "SCSS"],
-    link: "https://www.google.com",
+    link: "https://rev3al-dapp.vercel.app/",
   },
   {
     title: "DrivenX Dashboard",
@@ -59,7 +59,7 @@ const projects = [
     description:
       "A dashboard for holders of the DrivenX token, that displays the stats of the token, developer wallet transactions, and more.",
     tools: ["Vue.js", "JavaScript", "SCSS"],
-    link: "https://www.google.com",
+    link: "https://drivenx-dashboard.netlify.app/",
   },
 ];
 
@@ -69,20 +69,26 @@ export default function Projects() {
     <section className={styles.projectsContainer}>
       <SharedNav pathname={pathname} />
       <div className={styles.gridRight}>
+        <span className={styles.swipe}>
+          <span className={styles.path} />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M4.021 10.688c1.208.172 2.51 1.312 2.979 1.781v-10.514c0-1.08.92-1.955 2-1.955s2 .875 2 1.955v6.058c0 .784.814.885.919.103.216-1.604 2.519-1.817 2.693.399.043.546.726.655.866.027.326-1.444 2.501-1.458 2.758.758.066.579.796.696.848.034.051-.67.281-.934.607-.934 1.098 0 2.309 2.019 2.309 4.41 0 4.295-3 4.306-3 11.19h-10c-.332-3.942-3.462-7.431-6.271-10.241-.488-.488-.729-1.052-.729-1.564 0-.93.759-1.688 2.021-1.507z" />
+          </svg>
+        </span>
         {projects.map((project, index) => (
-          <Link
-            href={project.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.project}
-            key={index}
-          >
+          <div className={styles.project} key={index}>
             <Image
               className={styles.projectImg}
               src={project.img}
               alt={project.title}
             />
-            <div className={styles.projectInfo}>
+            <Link
+              href={project.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={index}
+              className={styles.projectInfo}
+            >
               <h2>{project.title}</h2>
               <p>{project.description}</p>
               <div className={styles.tools}>
@@ -90,7 +96,7 @@ export default function Projects() {
                   <span key={index}>{tool}</span>
                 ))}
               </div>
-            </div>
+            </Link>
             <Link
               className={styles.link}
               href={project.link}
@@ -99,7 +105,7 @@ export default function Projects() {
             >
               Visit Website
             </Link>
-          </Link>
+          </div>
         ))}
       </div>
     </section>
